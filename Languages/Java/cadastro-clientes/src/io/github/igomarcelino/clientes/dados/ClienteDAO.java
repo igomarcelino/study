0924bc396fd6654174becc8ad4cdc17f1618cea5
Igo Marcelino <geslucian@hotmail.com>
2024-06-15 19:37:58 -0300

    mostrando clientes no banco

--- Languages/Java/cadastro-clientes/src/io/github/igomarcelino/clientes/dados/ClienteDAO.java
+++ Languages/Java/cadastro-clientes/src/io/github/igomarcelino/clientes/dados/ClienteDAO.java
@@ -1,26 +1,46 @@
 package io.github.igomarcelino.clientes.dados;
 
 import io.github.igomarcelino.clientes.dominio.Cliente;
+import io.github.igomarcelino.clientes.dominio.enums.TipoSexo;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 public class ClienteDAO {
 
     private  Connection conexaoSql;
 
+    // construtor para inicializar a conexao
     public ClienteDAO(Connection connection) {
         this.conexaoSql = connection;
     }
 
+    // comando SQL para inserir clientes no banco de dados
     private String inserirCliente = """
                 insert into clientes ( codigo, nome, sexo, cpf, idade) 
                 values (?, ?, ?, ?, ?)
             """; // ( string, string , enum, string,integer
+    private String atualizarCliente = """
+                UPDATE clientes
+                    set nome = ?,
+                        sexo = ?,
+                        idade = ?
+                        
+                    where cpf = ?
+            """;
+
+    private String buscarClientes = "select *from clientes";
+
+    private String deletarCliente = "DELETE from clientes where codigo = ?";
 
     public void inserirCliente (Cliente cliente){
 
+        // Como o PreparedStatement recebe uma conexaoSql ele ira pedir para tratar as SQLException
         try{
             PreparedStatement conexao = conexaoSql.prepareStatement(inserirCliente);
             conexao.setString(1,cliente.getCodigo().toString());
@@ -28,7 +48,60 @@ public class ClienteDAO {
             conexao.setString(3,cliente.getSexo().name());
             conexao.setString(4,cliente.getCpf());
             conexao.setInt(5,cliente.getIdade());
+            conexao.execute(); // executa o comando de insercao do cliente no banco
+
+        }catch (SQLException e){
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void atualizarCliente(Cliente cliente){
+
+
+        try{
+            PreparedStatement conexao = conexaoSql.prepareStatement(atualizarCliente);
+            conexao.setString(1,cliente.getNome());
+            conexao.setString(2,cliente.getSexo().name());
+            conexao.setInt(3,cliente.getIdade());
+            conexao.setString(4,cliente.getCpf());
             conexao.execute();
+
+        }catch (SQLException e) {
+            throw  new RuntimeException("Cliente nao encontrado");
+        }
+    }
+
+    public void deletarCliente(UUID uuid){
+        try {
+            PreparedStatement conexao = conexaoSql.prepareStatement(deletarCliente);
+            conexao.setString(1,uuid.toString());
+            conexao.execute();
+        }catch (SQLException e ){
+            throw new RuntimeException(e);
+        }
+    }
+    public void buscarClientes(){
+        List<Cliente> exibeClientes = new ArrayList<>();
+        try{
+            PreparedStatement conexao = conexaoSql.prepareStatement(buscarClientes);
+            ResultSet resultado = conexao.executeQuery();
+            while (resultado.next()){
+
+                String nome = resultado.getString("nome");
+                String cpf = resultado.getString("cpf");
+                String sexo = resultado.getString("sexo");
+                Integer idade = resultado.getInt("idade");
+
+                Cliente cliente = new Cliente();
+                cliente.setNome(nome);
+                cliente.setSexo(TipoSexo.valueOf(sexo));
+                cliente.setCpf(cpf);
+                cliente.setIdade(idade);
+
+                exibeClientes.add(cliente);
+            }
+
+            System.out.println(exibeClientes);
 
         }catch (SQLException e){
             throw new RuntimeException(e);
--- Languages/Java/cadastro-clientes/src/io/github/igomarcelino/clientes/dados/Main.java
+++ Languages/Java/cadastro-clientes/src/io/github/igomarcelino/clientes/dados/Main.java
@@ -4,17 +4,59 @@ import io.github.igomarcelino.clientes.dominio.Cliente;
 import io.github.igomarcelino.clientes.dominio.enums.TipoSexo;
 
 import java.sql.SQLException;
+import java.util.UUID;
 
 public class Main{
     public static void main(String[] args) throws SQLException {
 
+        mostrarClientes();
+    }
+
+    private static void atualizarCliente(String nome, TipoSexo tipoSexo,Integer idade, String cpf) throws SQLException {
+        var conexao = FabricaConexoes.criarConexao();
+        ClienteDAO clienteDAO = new ClienteDAO(conexao);
+        // criando um novo cliente baseado em um que ja exista para ser atualizado
+        Cliente clienteParaAtualizar = new Cliente();
+        clienteParaAtualizar.setNome(nome);
+        clienteParaAtualizar.setSexo(tipoSexo);
+        clienteParaAtualizar.setIdade(idade);
+        clienteParaAtualizar.setCpf(cpf);
+
+        clienteDAO.atualizarCliente(clienteParaAtualizar);
+
+        conexao.close();
+    }
+
+    private static void inserirCliente() throws SQLException {
         var  conexao = FabricaConexoes.criarConexao();
 
-        ClienteDAO daoCLiente = new ClienteDAO(conexao);
+        ClienteDAO daoCLiente = new ClienteDAO(conexao); // abre conexao com o banco
 
-        Cliente cliente = new Cliente("Juca", "12333322211", TipoSexo.M,new byte[]{},43);
-        daoCLiente.inserirCliente(cliente);
-        conexao.close();
+        Cliente cliente = new Cliente();
+        cliente.setNome("Igo");
+        cliente.setCpf("32333411121");
+        cliente.setSexo(TipoSexo.M);
+        cliente.setFoto(new byte[]{});
+        cliente.setIdade(22);
+        daoCLiente.inserirCliente(cliente); // utiliza o metodo inserir para inserir o cliente
+        conexao.close(); // fecha conexao com o banco
         System.out.println(cliente);
     }
+
+    private static void deletarCLiente() throws SQLException {
+
+        var conexao = FabricaConexoes.criarConexao();
+        ClienteDAO clienteDAO = new ClienteDAO(conexao);
+
+        clienteDAO.deletarCliente(UUID.fromString("94cf6319-435b-4995-ae9c-6b976ddcc125"));
+        conexao.close();
+    }
+
+    private static void mostrarClientes(){
+        var conexao = FabricaConexoes.criarConexao();
+        ClienteDAO clienteDAO = new ClienteDAO(conexao);
+
+        clienteDAO.buscarClientes();
+    }
+
 }

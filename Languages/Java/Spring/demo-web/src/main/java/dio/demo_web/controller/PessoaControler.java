22be5208b44349745ce2550b1ba339a99f332ba8
Igo Marcelino <geslucian@hotmail.com>
2024-11-08 12:02:18 -0300

    Estudos sobre RestControler, aprofundando Mappings

--- Languages/Java/Spring/demo-web/src/main/java/dio/demo_web/controller/PessoaControler.java
+++ Languages/Java/Spring/demo-web/src/main/java/dio/demo_web/controller/PessoaControler.java
@@ -3,10 +3,7 @@ package dio.demo_web.controller;
 import dio.demo_web.model.Pessoa;
 import dio.demo_web.repository.PessoaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -25,4 +22,19 @@ public class PessoaControler {
     public Pessoa getPessoa(@PathVariable("nome") String nome){
         return pessoaRepository.findPessoa(nome);
     }
+
+    @DeleteMapping("/pessoas/{nome}")
+    public void deletePessoa(@PathVariable("nome") String nome){
+        pessoaRepository.deletePessoa(nome);
+    }
+    @PostMapping("/pessoas")
+    public void postPessoa(@RequestBody Pessoa pessoa){
+        pessoaRepository.savePessoa(pessoa);
+    }
+
+    @PutMapping("/pessoas")
+    public void putPessoa(@RequestBody Pessoa pessoa){
+        pessoaRepository.updatePessoa(pessoa);
+    }
+
  }
--- Languages/Java/Spring/demo-web/src/main/java/dio/demo_web/repository/PessoaRepository.java
+++ Languages/Java/Spring/demo-web/src/main/java/dio/demo_web/repository/PessoaRepository.java
@@ -2,7 +2,6 @@ package dio.demo_web.repository;
 
 import dio.demo_web.model.Pessoa;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -10,15 +9,17 @@ import java.util.List;
 @Repository
 public class PessoaRepository  {
 
-    public List<Pessoa> findAll(){
+    List<Pessoa> pessoaList = new ArrayList<>();
+    public PessoaRepository(){
         Pessoa pessoa = new Pessoa("Igo","13299922210","13998882288");
         Pessoa pessoa1 = new Pessoa("Julio","89211109100","14928291122");
         Pessoa pessoa2 = new Pessoa("Aline","8332229911","11977882299");
-        List<Pessoa> pessoaList = new ArrayList<>();
+
         pessoaList.add(pessoa);
         pessoaList.add(pessoa1);
         pessoaList.add(pessoa2);
-
+    }
+    public List<Pessoa> findAll(){
         return pessoaList;
 
     }
@@ -27,5 +28,30 @@ public class PessoaRepository  {
         List<Pessoa> pessoaList = findAll();
         return pessoaList.stream().
                 filter(n -> n.getNome().equalsIgnoreCase(nome)).findAny().get();
+    }
+
+    public void deletePessoa(String nome){
+        Pessoa pessoa = pessoaList.
+                stream().
+                filter(p->p.getNome().equalsIgnoreCase(nome)).
+                findAny().
+                get();
+        pessoaList.remove(pessoa);
+    }
+
+    public void savePessoa(Pessoa pessoa){
+        pessoaList.add(pessoa);
+    }
+
+    public void updatePessoa(Pessoa pessoa){
+        int index = 0;
+        for (Pessoa p : pessoaList){
+            if(p.getNome().equalsIgnoreCase(pessoa.getNome())){
+                index = pessoaList.indexOf(p);
+                pessoaList.set(index,pessoa);
+                break;
+            }
+        }
+
     }
 }

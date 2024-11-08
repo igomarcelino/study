f82d06f7b7fc0a46bd2dc34366d34565b6e2e4a6
Igo Marcelino <geslucian@hotmail.com>
2024-11-08 15:40:22 -0300

    Estudos sobre RestControler, Habilitando o Swagger na aplicacao

--- Languages/Java/Spring/demo-web/src/main/java/dio/demo_web/controller/PessoaControler.java
+++ Languages/Java/Spring/demo-web/src/main/java/dio/demo_web/controller/PessoaControler.java
@@ -2,6 +2,8 @@ package dio.demo_web.controller;
 
 import dio.demo_web.model.Pessoa;
 import dio.demo_web.repository.PessoaRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,25 +15,30 @@ public class PessoaControler {
     @Autowired
     PessoaRepository pessoaRepository;
 
+    @Operation(summary = "Listar todas as pessoas")
     @GetMapping("/pessoas")
     public List<Pessoa> getPessoas(){
         return pessoaRepository.findAll();
     }
 
+    @Operation(summary = "Listar pessoa por nome")
     @GetMapping("/pessoas/{nome}")
     public Pessoa getPessoa(@PathVariable("nome") String nome){
         return pessoaRepository.findPessoa(nome);
     }
 
+    @Operation(summary = "Deletar uma Pessoa")
     @DeleteMapping("/pessoas/{nome}")
     public void deletePessoa(@PathVariable("nome") String nome){
         pessoaRepository.deletePessoa(nome);
     }
+    @Operation(summary = "Salvar uma pessoa")
     @PostMapping("/pessoas")
     public void postPessoa(@RequestBody Pessoa pessoa){
         pessoaRepository.savePessoa(pessoa);
     }
 
+    @Operation(summary = "Atualiar Pessoa")
     @PutMapping("/pessoas")
     public void putPessoa(@RequestBody Pessoa pessoa){
         pessoaRepository.updatePessoa(pessoa);
